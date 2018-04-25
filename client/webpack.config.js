ddaf71e31d6af55ddf02722dc535ef13b7b3451b
Roman Voloboev <animir@ya.ru>
2018-04-25 16:51:57 +1000

    Added: redux

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 const config = {
   entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, 'public'),
+    path: path.resolve(__dirname, 'public/dist'),
     filename: 'index.js',
   },
   module: {
